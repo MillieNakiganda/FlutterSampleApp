92355fce1fadba8047816fcac89421dd7466d162
Millie Nakiganda <millienakiganda@gmail.com>
2022-08-23 17:55:23 +0300

    ch (refactor): make every screen a file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample/screens/intro_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,32 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Naki Fitness')),
-        body: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage('assets/beach.jpg'),
-              fit: BoxFit.cover,
-            )),
-            child: Center(
-                child: Container(
-                    padding: const EdgeInsets.all(24),
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      color: Colors.white70,
-                    ),
-                    child: const Text(
-                      'Commit to be fit, Care to be great',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 22, shadows: [
-                        Shadow(
-                            offset: Offset(1.0, 1.0),
-                            blurRadius: 2.0,
-                            color: Colors.grey)
-                      ]),
-                    )))),
-      ),
-    );
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        home: const IntroScreen());
   }
 }
