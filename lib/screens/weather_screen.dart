dd0451dc5e6e7ff97e2f0c204847089f9dc92799
Millie Nakiganda <millienakiganda@gmail.com>
2022-08-26 16:04:03 +0300

    ch: populate Weather UI with response result

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -20,23 +20,68 @@ class _WeatherState extends State<WeatherScreen> {
       pressure: 0,
       humidity: 0);
 
+  final TextEditingController txtPlace = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Weather')),
-      drawer: const MenuDrawer(),
-      body: Column(
-        children: [
-          ElevatedButton(onPressed: getData, child: const Text('Get Data')),
-          Text(weatherresult.name),
-        ],
-      ),
-    );
+        appBar: AppBar(title: const Text('Weather')),
+        drawer: const MenuDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(18),
+          child: ListView(
+            children: [
+              Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: txtPlace,
+                    decoration: InputDecoration(
+                        hintText: 'Enter a city',
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.search),
+                          onPressed: getData,
+                        )),
+                  )),
+              weatherRow('Place', weatherresult.name),
+              weatherRow('Description', weatherresult.description),
+              weatherRow(
+                  'Temperature', weatherresult.temperature.toStringAsFixed(2)),
+              weatherRow(
+                  'Temperature', weatherresult.perceived.toStringAsFixed(2)),
+              weatherRow('Pressure', weatherresult.pressure.toStringAsFixed(2)),
+              weatherRow('humidity', weatherresult.humidity.toStringAsFixed(2)),
+            ],
+          ),
+        ));
   }
 
   Future getData() async {
     HttpHelper helper = HttpHelper();
-    weatherresult = await helper.getWeather('London');
+    weatherresult = await helper.getWeather(txtPlace.text);
     setState(() {});
+  }
+
+  Widget weatherRow(String label, String value) {
+    Widget row = Padding(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        child: Row(
+          children: [
+            Expanded(
+                flex: 3,
+                child: Text(label,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).hintColor,
+                    ))),
+            Expanded(
+                flex: 4,
+                child: Text(value,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).primaryColor,
+                    )))
+          ],
+        ));
+    return row;
   }
 }
